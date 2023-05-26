a5c3369bae47603897d598f9d739a5084fea7fef
mensafri <safrijawad1245@gmail.com>
2023-05-26 07:02:44 +0700

    Routing + React-Router

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
-import DirectoryComponent from "./components/directory/DirectoryComponent";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/Home";
+import Navigation from "./routes/navigation/Navigation";
+
+const Shop = () => {
+  return (
+    <div>
+      <h2>Shop Page</h2>
+    </div>
+  )
+}
+
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-  return <DirectoryComponent categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/routes/navigation/Navigation.jsx
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import './navigation.styles.scss'
+
+export default function Navigation() {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+}
