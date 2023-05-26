8e7b68ee6d84c992d1883937392824f7c26861e9
mensafri <safrijawad1245@gmail.com>
2023-05-26 21:06:43 +0700

    Sign In with Redirect

---
+++ src/utils/firebase/firebase.js
@@ -0,0 +1,61 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAC1zho05t_IKIgd-vw13qkLGCRLvFxRi4",
+  authDomain: "capstone-ecomerce.firebaseapp.com",
+  projectId: "capstone-ecomerce",
+  storageBucket: "capstone-ecomerce.appspot.com",
+  messagingSenderId: "909189396006",
+  appId: "1:909189396006:web:ed6a5284e90c6af5b6d8e7",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  const userSnapShot = await getDoc(userDocRef);
+
+  if (!userSnapShot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error di: ", error.message);
+    }
+  }
+
+  return userDocRef;
+};
