66f69d29e7173d3d82ce75840f251311745f72a7
mensafri <safrijawad1245@gmail.com>
2023-06-07 05:03:36 +0700

    appply redux dev tools

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,35 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
 
-const middleWares = [logger];
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
+);
 
-const composedEnhancer = compose(applyMiddleware(...middleWares));
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
 
-export const store = createStore(rootReducer, undefined, composedEnhancer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const composedEnhancer =
+  compose(
+    process.env.NODE_ENV !== "production" &&
+      window &&
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ) || compose;
+
+const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares));
+
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
