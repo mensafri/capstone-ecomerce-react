dba4e00b6c4dfd6af9f548b960d2d0b32c3b699a
mensafri <safrijawad1245@gmail.com>
2023-06-10 08:10:21 +0700

    fixing default export function

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useReducer, useState } from "react";
-import createAction from "../utils/reducer/reducer.utils";
+import {createAction} from "../utils/reducer/reducer.utils";
 
 const addCartItem = (cartItems, productToAdd) => {
   const existingCartItem = cartItems.find(
